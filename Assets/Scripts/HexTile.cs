13ba485673bfb891375331e9a825264a31192c8e
YFOMNN <48487037+YFOMNN@users.noreply.github.com>
2025-03-29 16:45:45 +0400

    Changing

---
+++ Assets/Scripts/HexTile.cs
@@ -0,0 +1,48 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTile : MonoBehaviour
+{
+    public List<Transform> neighbours = new List<Transform> ();
+    private enum TerrainType
+    {
+        Ocean,
+        Forest,
+        Quarry,
+        River,
+        Mountain,
+        Town
+    }
+    private TerrainType type;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        while (GameManager.instance == null) { }
+        if (gameObject.name.Contains("Hills"))
+            type = TerrainType.Quarry;
+        else if (gameObject.name.Contains("Forest"))
+            type = TerrainType.Forest;
+        else if (gameObject.name.Contains("Ocean"))
+            type = TerrainType.Ocean;
+        else if (gameObject.name.Contains("River"))
+            type = TerrainType.River;
+        else if (gameObject.name.Contains("Mountain"))
+            type = TerrainType.Mountain;
+        else if (gameObject.name.Contains("Town"))
+            type = TerrainType.Town;
+        GameManager.instance.hexTiles.Add(transform);
+    }
+    void Start()
+    {
+        while (!GameManager.instance.ready) { }
+
+        neighbours = GameManager.instance.getNeighbours(transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
