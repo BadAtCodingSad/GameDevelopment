2699691e8ac9640cb84ebf850821af2480f2a307
YFOMNN <48487037+YFOMNN@users.noreply.github.com>
2025-04-03 15:10:02 +0400

    WorkerSystem

--- Assets/Scripts/HexTile.cs
+++ Assets/Scripts/HexTile.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class HexTile : MonoBehaviour
 {
     public List<Transform> neighbours = new List<Transform> ();
+    public TerrainResource terrainResource;
+    private GameManager gameManager;
+
+    public int metal;
+    public int wood;
+    public int oil;
+    public int fish;
+
+    public int workersOnTile;
+
     private enum TerrainType
     {
         Ocean,
@@ -31,15 +41,27 @@ public class HexTile : MonoBehaviour
             type = TerrainType.Mountain;
         else if (gameObject.name.Contains("Town"))
             type = TerrainType.Town;
-        GameManager.instance.hexTiles.Add(transform);
+        gameManager = GameManager.instance;
+        gameManager.hexTiles.Add(transform);
     }
     void Start()
     {
         while (!GameManager.instance.ready) { }
         if (type == TerrainType.Town)
-            GameManager.instance.townTile = this;
+            gameManager.townTile = this;
 
-        neighbours = GameManager.instance.getNeighbours(transform);
+        neighbours = gameManager.getNeighbours(transform);
+        foreach(TerrainResource terrainResource in gameManager.terrainResources) 
+        {
+            if (terrainResource.type.ToString() == type.ToString()) 
+            {
+                metal = Mathf.FloorToInt(Random.Range(terrainResource.metal.x, terrainResource.metal.y));
+                wood = Mathf.FloorToInt(Random.Range(terrainResource.wood.x, terrainResource.wood.y));
+                oil = Mathf.FloorToInt(Random.Range(terrainResource.oil.x, terrainResource.oil.y));
+                fish = Mathf.FloorToInt(Random.Range(terrainResource.fish.x, terrainResource.fish.y));
+
+            }
+        }
     }
 
     // Update is called once per frame
